247ba26f67126f04c1d822d81c6046e6f5682a29
mert <>
2022-09-23 18:49:37 +0300

    redux toolkit

---
+++ src/features/UserSlice.js
@@ -0,0 +1,24 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
+      currentUser: null,
+}
+
+export const UserSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        loginStart: (state)=> {
+            state.currentUser = null;
+        },
+        loginSuccess: (state, action) => {
+            state.currentUser = action.payload;
+        },
+        logout: (state) => {
+            state.currentUser = null;
+        }
+    }
+});
+
+export const UserReducer = UserSlice.reducer;
+export const {loginStart, loginSuccess, logout} = UserSlice.actions;
---
+++ src/store/store.js
@@ -0,0 +1,16 @@
+import {configureStore, /*combineReducers*/} from "@reduxjs/toolkit";
+import {UserReducer} from "../features/UserSlice.js";
+
+/* const rootReducers = combineReducers({
+       userReducer,
+       postReducer,
+       commentReducer,
+})*/
+
+const store = configureStore({
+    reducer: {
+       user: UserReducer,
+    }
+});
+
+export default store;
